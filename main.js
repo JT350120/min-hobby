687506b91106ea6ccf4f337768af1c507f9f3f68
Johan <johantoerme@live.se>
2024-03-26 22:55:18 +0100

    :feat: Added eventlisteners on buttons

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
 import './style.css'
+
+const dataBtn = document.querySelector("#dataBtn");
+const galleryBtn = document.querySelector("#galleryBtn");
+const historyBtn = document.querySelector("#historyBtn");
+
+const contentDiv = document.querySelector("#content");
+
+dataBtn.addEventListener("click", createDataDiv);
+galleryBtn.addEventListener("click", createGalleryDiv);
+historyBtn.addEventListener("click", createHistoryDiv);
+
+function createDataDiv() {
+  contentDiv.textContent = 'data';
+}
+
+function createGalleryDiv() {
+  contentDiv.textContent = 'gallery';
+}
+
+function createHistoryDiv() {
+  contentDiv.textContent = 'history';
+}
