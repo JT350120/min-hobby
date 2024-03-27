2a87c26b6967f5bc052b3c69630b93b06b8d2b7a
Johan <johantoerme@live.se>
2024-03-27 21:16:23 +0100

    :sparkles: Printing out the technical specifications with createDataContent()

--- main.js
+++ main.js
@@ -9,17 +9,36 @@ const contentDiv = document.querySelector("#content");
 const picDiv = document.querySelector("#picDiv");
 const textDiv = document.querySelector("#textDiv");
 
-dataBtn.addEventListener("click", createDataDivs);
-galleryBtn.addEventListener("click", createGalleryDivs);
-historyBtn.addEventListener("click", createHistoryDivs);
+dataBtn.addEventListener("click", createDataContent);
+galleryBtn.addEventListener("click", createGalleryContent);
+historyBtn.addEventListener("click", createHistoryContent);
 
 function createStartPageContent() {
-  picDiv.innerText = "Här kommer det en bild";
-  textDiv.innerText = "Dynamiskt skapad text på startsidan";
+  picDiv.textContent = "Här kommer det en bild";
+  textDiv.textContent = "Dynamiskt skapad text på startsidan";
 }
 
 function createDataContent() {
-  contentDiv.textContent = 'data';
+  picDiv.textContent = "Här kommer det en bild";
+  textDiv.textContent = "";
+  const dataListElement = document.createElement("ul");
+
+  const dataList = [
+    "Motor: 2-cylindrig, Rotax 440", 
+    "Växellåda: Ibland",
+    "Toppfart: Vet inte, hastighetsmätaren är trasig",
+    "Bromsar: Trasiga, sätt hälarna i snön",
+    "Stoppad sittdyna: Ja",
+    "Extrautrustning: Trasigt signalhorn"
+  ]
+
+  dataList.forEach((item) => {
+    const listItem = document.createElement("li");
+    listItem.textContent = item;
+    dataListElement.appendChild(listItem);
+  })
+
+  textDiv.appendChild(dataListElement);
 }
 
 function createGalleryContent() {
